80d616f7b67261fc74522461966265526c569cdf
Ignacio fq <ignacio@cobuilders.xyz>
2025-07-04 14:27:18 -0300

    Implement comprehensive unit tests for user contracts services
    - Added unit tests for UserContractsController and UserContractsService, ensuring robust coverage for contract creation, retrieval, updates, and deletions.
    - Introduced mock services to simulate dependencies, enhancing test reliability and isolation.
    - Developed tests for UserContractCrudService, UserContractValidationService, and UserContractEnrichmentService, validating their functionalities and error handling.
    - Created pagination and enrichment tests to ensure accurate responses and data integrity in user contract management.

--- src/user-contracts/user-contracts.controller.spec.ts
+++ src/user-contracts/user-contracts.controller.spec.ts
@@ -1,12 +1,29 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserContractsController } from './user-contracts.controller';
+import { UserContractsService } from './user-contracts.service';
 
 describe('UserContractsController', () => {
   let controller: UserContractsController;
+  let mockUserContractsService: Partial<UserContractsService>;
 
   beforeEach(async () => {
+    mockUserContractsService = {
+      createUserContract: jest.fn(),
+      getUserContracts: jest.fn(),
+      getUserContract: jest.fn(),
+      updateUserContractName: jest.fn(),
+      deleteUserContract: jest.fn(),
+      checkContractsSavedByUser: jest.fn(),
+    };
+
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UserContractsController],
+      providers: [
+        {
+          provide: UserContractsService,
+          useValue: mockUserContractsService,
+        },
+      ],
     }).compile();
 
     controller = module.get<UserContractsController>(UserContractsController);
